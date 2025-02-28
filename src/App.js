ef7b697d05b04d3c001961c73813ec01a27dc0f2
unknown <samehrizk1993@gmail.com>
2025-02-28 20:57:11 +0200

    adding App Nanbar section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import logo from "./logo.svg";
 import "./App.css";
+import AppNavbar from "./components/AppNavbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <AppNavbar />
+    </div>
+  );
 }
 
 export default App;
