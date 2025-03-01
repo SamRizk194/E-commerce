0b64f4b2569d68e7ef555f3d98b104c295647b32
unknown <samehrizk1993@gmail.com>
2025-03-01 15:08:50 +0200

    adding productslice and fetching products

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,4 +1,4 @@
 function Cart() {
-  return <h1> Hello From cart</h1>;
+  return <h1>welcome to cart</h1>;
 }
 export default Cart;
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,4 +1,37 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { Container, Row, Col } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { fetchProducts } from "../rtk/slices/product-slice";
+
 function Products() {
-  return <h1> Hello From Products</h1>;
+  const products = useSelector((state) => state.products);
+  const dispatch = useDispatch();
+  console.log(products);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+  return (
+    <Container>
+      <Row>
+        {products.map((product) => {
+          return (
+            <Col key={product.id}>
+              <Card style={{ width: "18rem" }}>
+                <Card.Img variant="top" src={product.image} />
+                <Card.Body>
+                  <Card.Title>{product.title}</Card.Title>
+                  <Card.Text>{product.description}</Card.Text>
+                  <Button variant="primary">Add to Cart</Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
 }
 export default Products;
